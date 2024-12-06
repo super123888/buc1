db044249e65287095fdc7ab387771e939f4ea043
super123888 <buatawan2@gmail.com>
2024-12-06 14:32:56 +0700

    Update buc2.sh

--- buc2.sh
+++ buc2.sh
@@ -1,5 +1,5 @@
 #vrs
 sudo apt update
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 ./helminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RAYBustGzXSgdiKNEteTKf6PiKoVit4PT4.bang1 -p x --cpu 4
